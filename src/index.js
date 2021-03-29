f4aa170bb6c95c9e296c043dc7380642e3ddbcc6
ShahbazVK <63925374+ShahbazVK@users.noreply.github.com>
2021-03-29 14:14:45 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import App from './App'
+<link rel="stylesheet" href="index.css"/>
+  import React from "react";
+import ReactDOM from "react-dom";
 ReactDOM.render(
   <App/>,
   document.getElementById('root')
